5394ad1f8e8dcf1545f994b6229f626e456e1cbd
DQ26525 <DQ26525@LT8085.Randers.dk>
2019-05-12 20:00:03 +0200

    Added facet navigation

--- templates/ting-object-tree-page.tpl.php
+++ templates/ting-object-tree-page.tpl.php
@@ -1,13 +1,20 @@
-<?php
-
-?>
+<?php ?>
 <div class="frontpage-layout">
     <div class="panel-pane pane-node-content">
+        
+        <ul class="ting-object-tree-links">
+            <?php foreach ($links as $link) : ?>
+              <li class="ting-object-tree-link">
+                  <?php print $link; ?> 
+              </li>
+
+            <?php endforeach; ?>
+        </ul>
         <ul class="ting-object-tree">
             <?php foreach ($items as $item) : ?>
-                <li class="ting-object-tree-item">
-                    <?php print $item; ?> 
-                </li>
+              <li class="ting-object-tree-item">
+                  <?php print $item; ?> 
+              </li>
 
             <?php endforeach; ?>
         </ul>
