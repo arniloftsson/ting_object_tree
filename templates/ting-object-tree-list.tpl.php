697a521e99be75dc3c295d21161ac25e064dbbf6
DQ26525 <DQ26525@LT8085.Randers.dk>
2020-08-24 13:34:15 +0200

    Added smart carasoul page

---
+++ templates/ting-object-tree-list.tpl.php
@@ -0,0 +1,10 @@
+<?php ?>
+
+        <ul class="ting-object-tree">
+            <?php foreach ($items as $item) : ?>
+              <li class="ting-object-tree-item">
+                  <?php print $item; ?> 
+              </li>
+
+            <?php endforeach; ?>
+
--- templates/ting-object-tree-page.tpl.php
+++ templates/ting-object-tree-page.tpl.php
@@ -9,14 +9,15 @@
         <div class="ting-object-tree-breadcrumb">
             <?php print $breadcrumb; ?> 
         </div>
-        <ul class="ting-object-tree-links">
+        <h2 class="pane-title"><?php print $title; ?> </h2>
+<!--        <ul class="ting-object-tree-links">
             <?php foreach ($links as $link) : ?>
               <li class="ting-object-tree-link">
                   <?php print $link; ?> 
               </li>
 
             <?php endforeach; ?>
-        </ul>
+        </ul>-->
         <ul class="ting-object-tree">
             <?php foreach ($items as $item) : ?>
               <li class="ting-object-tree-item">
