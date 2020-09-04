f793c7ed2fe9fec720ee9ebe3393560846b66beb
DQ26525 <DQ26525@LT8085.Randers.dk>
2020-09-04 16:42:12 +0200

    Css styling, search field

--- templates/ting-object-tree-list.tpl.php
+++ templates/ting-object-tree-list.tpl.php
@@ -1,5 +1,11 @@
 <?php ?>
+<?php if(isset($title)) : ?>
+<div class="ting-object-tree-title">
+    
+    <h2 class="pane-title"><?php print $title; ?> </h2>
 
+</div>
+<?php endif ?>
 <ul class="ting-object-tree">
     <?php foreach ($items as $item) : ?>
       <li class="ting-object-tree-item">
