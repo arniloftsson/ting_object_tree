7e873e61895dedbf1368a522e0a34a450d6ca223
vagrant <vagrant@drupalvm4.test>
2021-05-04 13:45:44 +0000

    Sorted noncovers last

--- js/ting-object-tree.js
+++ js/ting-object-tree.js
@@ -1,20 +1,23 @@
-(function($) {
-    $(document).ready(function() {
-           $.post("/ting/objecttreesearch", Drupal.settings.tingObjectTreeSearch.active, SearchExpansionResults);
-console.log("test");
-console.log(Drupal.settings.tingObjectTreeSearch.active);
-      }); 
-    
-     var SearchExpansionResults= function(response) {
-console.log(response);
-        $.each (response, function(key, value ) {
+(function ($) {
+    $(document).ready(function () {
+        $.post("/ting/objecttreesearch", Drupal.settings.tingObjectTreeSearch.active, SearchExpansionResults);
+        console.log("test");
+        console.log(Drupal.settings.tingObjectTreeSearch.active);
+    });
+
+    var SearchExpansionResults = function (response) {
+        console.log(response);
+        $.each(response, function (key, value) {
             //var text = $( "#ting-search-expansion-link-" + key ).text().replace('...', value); 
-            var li = $( "#" + key + " > a:first > span:first" );
-            console.log( li );
-            li.append(" " + value);
+            var li = $("#" + key + " > a:first");
+            console.log(li);
+            //li.append(value);
+            if (value > 0) {
+                li.append('<span class="badge"> ' + value + '</span>');
+            }
         });
-     }
-        
+    }
+
 
 
 })(jQuery);
